163605aebd7d9aa44f64cdbca28e2745114257c9
OmreFrauk <117479359+OmreFrauk@users.noreply.github.com>
2024-06-03 14:47:31 +0300

    implemented user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto): Promise<void> {
+    await this.userService.register(createUserDto);
+  }
+  @Get('verify/:username/:verificationToken')
+  async verifyEmail(
+    @Param('username') username: string,
+    @Param('verificationToken') verificationToken: string,
+  ): Promise<string> {
+    const verified = await this.userService.verifyEmail(
+      username,
+      verificationToken,
+    );
+    if (!verified) {
+      throw new BadRequestException('Invalid verification token');
+    } else {
+      return 'Email verified';
+    }
+  }
+  @Get('check-verification/:username')
+  async checkVerification(
+    @Param('username') username: string,
+  ): Promise<string> {
+    const isVerified = await this.userService.checkVerification(username);
+    if (!isVerified) {
+      return 'User is not verified';
+    }
+    return 'User is verified';
+  }
+}
